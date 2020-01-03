294fb560ff27f6e2b26b99a046c0d2d04bdcd70d
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2020-01-02 22:32:02 -1000

    Reconfigured 'face book' to properly appear on face iamges.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
   return (
     <div>
       <p className='f3'>
-      {'Please put in a .png below to detect faces!'}
+      {'Please put in a .jpg below to detect faces!'}
       </p>
       <div className='center'>
         <div className='form center pa2 br3 shadow-5'>
-        <input className='f4 pa2 w-70 center' type='tex' />
-      <button className='w-30 grow f4 link ph3 pv1 dib white bg-light-blue'>Detect</button>
+        <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
+      <button
+      className='w-30 grow f4 link ph3 pv1 dib white bg-light-blue'
+      onClick={onButtonSubmit}
+      >Detect</button>
+
         </div>
       </div>
     </div>
