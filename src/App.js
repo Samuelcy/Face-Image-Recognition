deff88d8f28f3656c04dd70584ee6765f9a047f8
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2020-01-02 22:32:02 -1000

    Reconfigured 'face book' to properly appear on face iamges.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
 
+
+const app = new Clarifai.App({
+ apiKey: '01e3851073a84a2397d4aa561afe6ff4'
+});
 
 const particleOptions = {
   particles: {
@@ -20,18 +26,61 @@ const particleOptions = {
   }
 }
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+}
+
+
+render() {
   return (
     <div className="App">
-    <Particles className='particles'
-      params={particleOptions}
-     />
-      <Navigation/>
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
+    <Particles className='particles' params={particleOptions}/>
+    <Navigation/>
+    <Logo />
+    <Rank />
+    <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+    <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
